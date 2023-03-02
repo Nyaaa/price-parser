6a4a74247b7d8385426180dfada1989d0d1f5b50
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2023-03-02 13:50:48 +0300

    logging

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ import csv
 import asyncio
 import aiohttp
 import time
+import coloredlogs
+import logging
 
 load_dotenv(find_dotenv())
+logger = logging.getLogger(__name__)
+coloredlogs.install(level='DEBUG')
 
 
 class Parser:
@@ -18,53 +22,50 @@ class Parser:
 
     async def get_categories(self):
         """Get category links from main page."""
-        links = set()
-        async with self.session.get(url=self.URL) as response:
-            response_text = await response.text()
-            menu = SoupStrainer('div', id='mainmenu')
-            soup = BeautifulSoup(response_text, 'lxml', parse_only=menu)
-            for category in soup.find_all(href=True):
-                links.add(category.get('href'))
-            return links
+        response_text = await self.fetch(self.URL)
+        menu = SoupStrainer('div', id='mainmenu')
+        soup = BeautifulSoup(response_text, 'lxml', parse_only=menu)
+        links = [category.get('href') for category in soup.find_all(href=True)]
+        await self.add_tasks(links)
+
+    async def fetch(self, url):
+        async with self.session.get(url) as response:
+            logging.debug(f'Fetching page {url}...')
+            return await response.text()
 
     async def scrape(self, category):
         if category in self.done:
             return
         page = f'{self.URL}{category}?product_all=1'
         self.done.append(category)
-
-        async with self.session.get(url=page) as response:
-            response_text = await response.text()
-            soup = BeautifulSoup(response_text, 'lxml')
-
-            cards = soup.find_all('div', attrs={'class': 'pad'})
-            cats = soup.find_all('div', attrs={'class': 'category'})
-            if cats:
-                new_links = [i.a['href'] for i in cats]
-                add_links = [i for i in new_links if 'category' in i]
-                await self.add_tasks(add_links)
-            else:
-                # self.done.append(category)
-                title = soup.title.string.split(' - ')[0]
-                cat_data = {title: {}}
-                for card in cards:
-                    article = card.find('div', attrs={'class': 'art'}).get_text().split(' ', 1)[1]
-                    price = card.find('div', attrs={'class': 'price'}).get_text().encode('ascii', 'ignore')
-                    cat_data[title][article] = float(price)
-                self.data.update(cat_data)
-                print(category)
+        response_text = await self.fetch(page)
+        logging.debug(f'Fetched page {page}')
+        soup = BeautifulSoup(response_text, 'lxml')
+        cards = soup.find_all('div', attrs={'class': 'pad'})
+        cats = soup.find_all('div', attrs={'class': 'category'})
+        if cats:
+            new_links = [i.a['href'] for i in cats]
+            add_links = [i for i in new_links if 'category' in i]
+            await self.add_tasks(add_links)
+        else:
+            title = soup.title.string.split(' - ')[0]
+            cat_data = {title: {}}
+            for card in cards:
+                article = card.find('div', attrs={'class': 'art'}).get_text().split(' ', 1)[1]
+                price = card.find('div', attrs={'class': 'price'}).get_text().encode('ascii', 'ignore')
+                cat_data[title][article] = float(price)
+            self.data.update(cat_data)
+            logging.info(f'Parsed {category}')
 
     async def add_tasks(self, links):
-        async with asyncio.TaskGroup() as tg:
-            for cat in links:
-                if cat not in self.done:
-                    tg.create_task(self.scrape(cat))
+        tasks = {self.scrape(cat) for cat in links if cat not in self.done}
+        await asyncio.gather(*tasks)
+        logging.debug(f'Added links: {links}')
 
     async def main(self):
         async with aiohttp.ClientSession() as session:
             self.session = session
-            links = await self.get_categories()
-            await self.add_tasks(links)
+            await self.get_categories()
 
         # wait for tasks to finish
         all_tasks = asyncio.all_tasks()
