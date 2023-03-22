e6ef6deb825e826b386c187b6f84248e6c878460
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2023-03-22 20:49:25 +0300

    async-retry

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import coloredlogs
 import logging
 import re
 import unicodedata
+from aiohttp_retry import RetryClient, ExponentialRetry
 
 load_dotenv(find_dotenv())
 logger = logging.getLogger(__name__)
@@ -19,9 +20,9 @@ class Parser:
     def __init__(self):
         self.URL = os.getenv('URL')
         if self.URL is None:
-            raise EnvironmentError('URL not specified')
+            raise EnvironmentError('URL not specified.')
         self.data = {}
-        self.done = []
+        self.done = set()
         self.session = None
 
     async def get_categories(self):
@@ -32,21 +33,20 @@ class Parser:
         links = [category.get('href') for category in soup.find_all(href=True)]
         await self.add_tasks(links)
 
-    async def fetch(self, url):
-        async with self.session.get(url, ssl=False) as response:
+    async def fetch(self, url: str):
+        retry_options = ExponentialRetry(attempts=5)
+        retry_client = RetryClient(client_session=self.session, retry_options=retry_options)
+        async with retry_client.get(url, ssl=False) as response:
             logger.debug(f'>>> Fetching page {url}')
-            try:
-                return await response.text()
-            except aiohttp.ClientPayloadError:
-                return await response.text()
+            return await response.text()
 
-    async def scrape(self, category):
+    async def scrape(self, category: str):
         if category in self.done:
             return
         page = f'{self.URL}{category}?product_all=1'
-        self.done.append(category)
         response_text = await self.fetch(page)
         logger.debug(f'<<< Fetched page {page}')
+        self.done.add(category)
         soup = BeautifulSoup(response_text, 'lxml')
         cards = soup.find_all('div', attrs={'class': 'pad'})
         cats = soup.find_all('div', attrs={'class': 'category'})
@@ -56,16 +56,18 @@ class Parser:
             await self.add_tasks(add_links)
         else:
             title = soup.title.string.split(' - ')[0]
+            title = unicodedata.normalize('NFKC', title)
             cat_data = {title: {}}
             for card in cards:
                 article = card.find('div', attrs={'class': 'art'}).get_text().split(' ', 1)[1]
+                article = unicodedata.normalize('NFKC', article)
                 price = card.find('div', attrs={'class': re.compile(r'^price')}).get_text()
-                price = unicodedata.normalize('NFKD', price).replace(' ', '')
+                price = unicodedata.normalize('NFKC', price).replace(' ', '')
                 cat_data[title][article] = float(price)
             self.data.update(cat_data)
             logger.info(f'Parsed {category}')
 
-    async def add_tasks(self, links):
+    async def add_tasks(self, links: list[str]):
         links = {cat for cat in links if cat not in self.done}
         tasks = [self.scrape(link) for link in links]
         if tasks:
@@ -84,6 +86,7 @@ class Parser:
         if all_tasks:
             await asyncio.wait(all_tasks)
         else:
+            await self.session.close()
             self.export()
 
     def export(self):
